1398b75f2052bab7507133447d5a6a74b3a31fef
Aniket Soni <aniket271993@gmail.com>
2020-06-13 17:33:16 -0400

    Added Heroku settings

--- weather/settings.py
+++ weather/settings.py
@@ -1,4 +1,8 @@
 import os
+import django_heroku
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
